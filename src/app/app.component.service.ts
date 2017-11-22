75269ef8124a2a39be11315a4ca25ee0ca441eae
Maiko Cunha <mkacunha@gmail.com>
2017-11-22 18:40:08 -0200

    Configurado url da api no environment

--- src/app/app.component.service.ts
+++ src/app/app.component.service.ts
@@ -4,6 +4,8 @@ import { Http, Headers } from '@angular/http';
 import { Message } from './message';
 import { User } from './User';
 
+import { environment } from '../environments/environment';
+
 @Injectable()
 export class AppComponentService {
 
@@ -12,17 +14,17 @@ export class AppComponentService {
   save(message: Message) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:8080/messages', message.toJson(), { headers: headers })
+    return this.http.post(`${environment.api}/messages`, message.toJson(), { headers: headers })
       .map(response => response.json() as Message);
   }
 
   findAllUsers() {
-    return this.http.get('http://localhost:8080/users')
+    return this.http.get(`${environment.api}/users`)
       .map(response => response.json() as User[]);
   }
 
   findUsersByNameContains(name: string) {
-    return this.http.get(`http://localhost:8080/users/contains/${name}`)
+    return this.http.get(`${environment.api}/users/contains/${name}`)
       .map(response => response.json() as User[]);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'http://192.168.208.164:8787'
 };
